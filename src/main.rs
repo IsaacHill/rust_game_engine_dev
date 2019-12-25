49065e6b7c6d030a10c5fa3b9d8d4acef64bb645
Isaac <isaachill13@gmail.com>
2019-12-25 21:06:06 +1000

    initial commit for rust math excersixes

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::ops::{Index,IndexMut};
+
+struct Vector3D {
+    x: f32,
+    y: f32,
+    z: f32,
+
+    
+}
+
+impl Index<u32> for Vector3D {
+    type Output = usize;
+
+    fn index(&self, i: f32) {
+        
+    }
+
+}
+impl Vector3D {
+    fn *(&self, u32: index) -> u32 {
+        self.width * self.height
+    }
+}
+
+
+
+fn main() {
+    let vector = Vector3D{ x:1,y:2,z3}
+}
+
---
+++ src/vector3d.rs
@@ -0,0 +1,13 @@
+struct Vector3D {
+    x: f32,
+    y: f32,
+    y: f32,
+
+    
+}
+
+impl Vector3D {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
